bb9bc46f13ecd136436f486a24a9254dcbd57154
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2025-01-22 10:13:49 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { useTemplateRef, reactive } from "vue";
+
+const inputField = useTemplateRef("input-field");
+
+const todo = reactive({
+  title: "",
+  isDone: false,
+});
+</script>
+
+<template>
+  <input
+    ref="input-field"
+    v-model="todo.title"
+    type="text"
+    placeholder="Add new todo..."
+  />
+  <button>Add</button>
+
+  <div>
+    <ul>
+      <li>Eat</li>
+      <li>Sleep</li>
+      <li>Gym</li>
+    </ul>
+  </div>
+
+  <span>{{ todo.title }}</span>
+</template>
+
+<style scoped></style>
---
+++ src/components/Counter.vue
@@ -0,0 +1,13 @@
+<script setup>
+import { ref } from "vue";
+
+let count = ref(0);
+</script>
+
+<template>
+  <h3>{{ count }}</h3>
+  <button @click="count++">+</button>
+  <button @click="count--">-</button>
+</template>
+
+<style scoped></style>
