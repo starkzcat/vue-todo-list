a05a06b86499e9979848867580bb5cb72be69aec
Andy Firmansyah <andyfirmansyah2667@gmail.com>
2025-01-23 22:22:16 +0700

    finished app

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,60 @@
 <script setup>
-import { useTemplateRef, reactive } from "vue";
+import { computed, ref } from "vue";
+import Todo from "./components/Todo.vue";
 
-const inputField = useTemplateRef("input-field");
-
-const todo = reactive({
-  title: "",
-  isDone: false,
+let id = 0;
+const hideCompleted = ref(false);
+const newTask = ref("");
+const tasks = ref([
+  {
+    id: id++,
+    title: "Eat",
+    completed: false,
+  },
+  {
+    id: id++,
+    title: "Sleep",
+    completed: false,
+  },
+  {
+    id: id++,
+    title: "Gym",
+    completed: true,
+  },
+]);
+const filteredTasks = computed(() => {
+  return hideCompleted.value
+    ? tasks.value.filter((task) => !task.completed)
+    : tasks.value;
 });
+
+function addTodo() {
+  tasks.value.push({ id: id++, title: newTask.value, completed: false });
+
+  newTask.value = "";
+}
+
+function deleteTodo(id) {
+  tasks.value = tasks.value.filter((task) => task.id !== id);
+}
+
+function clearTasks() {
+  tasks.value = [];
+}
 </script>
 
 <template>
-  <input
-    ref="input-field"
-    v-model="todo.title"
-    type="text"
-    placeholder="Add new todo..."
-  />
-  <button>Add</button>
+  <h1>Todo List App</h1>
+  <form @submit.prevent="addTodo">
+    <input type="text" placeholder="Add task" v-model="newTask" />
+    <button>Add</button>
+  </form>
+  <Todo :tasks="filteredTasks" @delete="(id) => deleteTodo(id)" />
 
-  <div>
-    <ul>
-      <li>Eat</li>
-      <li>Sleep</li>
-      <li>Gym</li>
-    </ul>
-  </div>
-
-  <span>{{ todo.title }}</span>
+  <button @click="() => (hideCompleted = !hideCompleted)">
+    {{ hideCompleted ? "Show All" : "Hide Done" }}
+  </button>
+  <button @click="clearTasks">Clear All</button>
 </template>
 
 <style scoped></style>
---
+++ src/components/Todo.vue
@@ -0,0 +1,40 @@
+<script setup>
+const { tasks } = defineProps(["tasks"]);
+const emit = defineEmits(["delete"]);
+</script>
+
+<template>
+  <div>
+    <ul>
+      <li v-for="task in tasks" :key="task.id">
+        <input
+          v-model="task.completed"
+          type="checkbox"
+          :checked="task.completed"
+          :id="task.id"
+        />
+        <label :for="task.id">
+          <span :class="{ done: task.completed }">{{ task.title }}</span>
+        </label>
+        <button @click="emit('delete', task.id)">
+          <i class="fa fa-trash-o"></i>
+        </button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<style scoped>
+.done {
+  text-decoration: line-through;
+}
+
+i {
+  color: red;
+  font-size: 16px;
+}
+
+button {
+  margin-left: 4px;
+}
+</style>
